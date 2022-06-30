2ceb8ec4623ebaf7976c7480fb56773684ab7d22
sourabhpatnaik <sourabhpatnayak19@gmail.com>
2022-06-30 17:59:44 +0530

    calculator

---
+++ 7. Calculator/calculator.js
@@ -0,0 +1,25 @@
+let outputScreen = document.getElementById("output-screen");
+
+function Display(num)
+{
+outputScreen.value +=  num;
+}
+
+function Calculate(){
+    try{
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch(err)
+    {
+        alert("Invalid")
+    }
+
+}
+
+function Clear(){
+    outputScreen.value = "";
+}
+
+function Del(){
+    outputScreen.value = outputScreen.value.slice(0,-1);
+}
